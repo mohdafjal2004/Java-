182432b870b2870b449761250747174154d2b763
Mohd Afjal <mohdafjalgci2018@gmail.com>
2023-04-03 17:36:00 +0530

    Added a simple class and accessed its propeties in Main Class

--- java/OOPS/Main.java
+++ java/OOPS/Main.java
@@ -17,18 +17,48 @@ public class Main {
 
         Student[] students = new Student[5];
 
-       Student afjal = new Student();
-       System.out.println(afjal.rollNo);
+        // While creating object afjal, If we are not specifying the properties given in
+        // Class Student
+        // then that property will display the default value as per
+        // datatype given in class
+        Student afjal = new Student();
+
+        // ? When using "this" keyword, internally at place of "this" keyword
+        // ? afjal will render like this
+        // afjal.rollNo = 13;
+        // afjal.name = "Mohd Afjal";
+        // afjal.marks = 87.6f;
+
+        afjal.greeting();
+
+        System.out.println(afjal.rollNo);
+        System.out.println(afjal.name);
+        System.out.println(afjal.marks);
+    }
+}
+
+// create a class
+// for every single student
+class Student {
+ 
+    int rollNo;
+    String name; 
+    float marks; 
+
+    // We need a way to add the values of the above
+    // properties object by object
+
+    // we need one word to access every object:this keyword
+    Student() {
+        this.rollNo = 13;
+        this.name = "Mohd Afjal";
+        this.marks = 779.48f;
 
     }
 
-    // create a class
-    // for every single student
-    class Student {
-
-        int rollNo;
-        String name;
-        float marks;
-
+    // function in template class
+    void greeting() {
+        System.out.println("My name is " + this.name);
     }
+
 }
