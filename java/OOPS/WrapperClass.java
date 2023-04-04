303811bec6ac9749b6f3bbc8650232d122ccce0c
Mohd Afjal <mohdafjalgci2018@gmail.com>
2023-04-04 10:04:21 +0530

    finale keyword and finalize method in Wrapper Class

--- java/OOPS/WrapperClass.java
+++ java/OOPS/WrapperClass.java
@@ -6,8 +6,37 @@ public class WrapperClass {
 
         Integer num = 45;// Here it is an object
 
-        final int bonus = 3;// Now this bonus can't be modified becoz of final keyword
+        // Use of final keyword for primitives
+        final int bonus = 3;// primitive datatypes like int ⁡⁢⁣⁢can't⁡ be modified
+
+        // Use of final keyword for non-primitives
+        final A afjal = new A("Mohd Afjal");
+        afjal.name = "Sonu";// changing the value is allowed ⁡⁢⁢
+        // afjal = new A("King");// reassiging is ⁡⁢⁣⁢not⁡ allowed
+
+        A obj;
+        for (int i = 0; i < 100000; i++) {
+            obj = new A("Random name");
+        }
 
     }
 
+}
+
+class A {
+    final int num = 10;
+    String name;
+
+    // Constructor
+    A(String name) {
+        System.out.println("Object is created");
+        this.name = name;
+    }
+
+    // Finalize method for handling the value manually whose object is destroyed
+    // automatically
+    @Override
+    protected void finalize() throws Throwable {
+        System.out.println("Object is destroyed");
+    }
 }
